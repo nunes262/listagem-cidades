396425d0e6957f57393dc13db798fd86a58e4690
nunes262 <pno.mco.jcn@gmail.com>
2023-04-13 21:06:07 -0300

    feat: finalizado a contrução do service

--- src/shared/services/api/pessoas/PessoasService.ts
+++ src/shared/services/api/pessoas/PessoasService.ts
@@ -39,10 +39,52 @@ const getAll = async (page = 1, filter = ''): Promise<TPessoasComTotalCount | Er
   }
 };
 
-const getById = async (): Promise<any> => { };
-const create = async (): Promise<any> => { };
-const updateById = async (): Promise<any> => { };
-const deleteById = async (): Promise<any> => { };
+const getById = async (id: number): Promise<IDetalhePessoa | Error> => {
+  try {
+    const { data } = await Api.get(`/pessoas/${id}`);
+
+    if (data) {
+      return data;
+    }
+
+    return new Error('Erro ao consultar o registro.');
+  } catch (error) {
+    console.error(error);
+    return new Error((error as { message: string }).message || 'Erro ao consultar o registro.');
+  }
+};
+
+const create = async (dados: Omit<IDetalhePessoa, 'id'>): Promise<number | Error> => {
+  try {
+    const { data } = await Api.post<IDetalhePessoa>('/pessoas', dados);
+
+    if (data) {
+      return data.id;
+    }
+    return new Error('Erro ao criar o registro.');
+  } catch (error) {
+    console.error(error);
+    return new Error((error as { message: string }).message || 'Erro ao criar o registro.');
+  }
+};
+
+const updateById = async (id: number, dados: IDetalhePessoa): Promise<void | Error> => {
+  try {
+    await Api.put(`/pessoas/${id}`, dados);
+  } catch (error) {
+    console.error(error);
+    return new Error((error as { message: string }).message || 'Erro ao atualizar o registro.');
+  }
+};
+
+const deleteById = async (id: number): Promise<void | Error> => {
+  try {
+    await Api.delete(`/pessoas/${id}`);
+  } catch (error) {
+    console.error(error);
+    return new Error((error as { message: string }).message || 'Erro ao apagar o registro.');
+  }
+};
 
 export const PessoasService = {
   getAll,
