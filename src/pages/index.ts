e998407c08f233e286504016f13768464ca571ac
nunes262 <pno.mco.jcn@gmail.com>
2023-04-17 08:45:08 -0300

    feat: Adicionado crud completo para cidades

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,5 @@
 export * from './dasboard/Dashboard';
 export * from './pessoas/ListagemDePessoas';
 export * from './pessoas/DetalheDePessoas';
+export * from './cidades/ListagemDeCidades';
+export * from './cidades/DetalheDeCidades';
